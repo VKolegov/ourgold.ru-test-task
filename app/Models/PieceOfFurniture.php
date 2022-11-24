2ff094e211d767ed8b8f9cdd35972e8d89dde9be
Vladislav Kolegov <v.kolegov@outlook.com>
2022-11-25 02:46:10 +0700

    [Models][DB] PieceOfFurniture: name property

--- app/Models/PieceOfFurniture.php
+++ app/Models/PieceOfFurniture.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
  * App\Models\PieceOfFurniture
  *
  * @property int $id
+ * @property string $name
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property string $type_code
