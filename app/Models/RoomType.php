e9ae8b54990bc3ddd7ff0bbb71fe44fe60b0faa9
Vladislav Kolegov <v.kolegov@outlook.com>
2022-11-24 23:22:17 +0700

    fxp roomtype

--- app/Models/RoomType.php
+++ app/Models/RoomType.php
@@ -5,6 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\RoomType
+ *
+ * @property int $id
+ * @property string $code
+ * @property string $name
+ * @method static \Illuminate\Database\Eloquent\Builder|RoomType newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|RoomType newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|RoomType query()
+ * @method static \Illuminate\Database\Eloquent\Builder|RoomType whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|RoomType whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|RoomType whereName($value)
+ * @mixin \Eloquent
+ */
 class RoomType extends Model
 {
     use HasFactory;
