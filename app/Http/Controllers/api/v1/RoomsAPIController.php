2ba1edcf9138d58f89c442675ce15a6562d90e71
Vladislav Kolegov <v.kolegov@outlook.com>
2022-11-25 14:09:47 +0700

    [Controllers] api/v1/RoomsAPIController@filterParams

--- app/Http/Controllers/api/v1/RoomsAPIController.php
+++ app/Http/Controllers/api/v1/RoomsAPIController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\api\v1;
 
+use App\Helpers\API\ExactMatchFieldFilter;
 use App\Models\Room;
 
 class RoomsAPIController extends AbstractAPIController
@@ -9,5 +10,13 @@ class RoomsAPIController extends AbstractAPIController
     protected function getModelClass(): string
     {
         return Room::class;
+    }
+
+    protected function filterParams(): array
+    {
+        return [
+            new ExactMatchFieldFilter('apartment_id', 'int'),
+            new ExactMatchFieldFilter('type_code', 'string'),
+        ];
     }
 }
