2ee51b7c5c8e778d83e68b99a7071498dcceab62
Vladislav Kolegov <v.kolegov@outlook.com>
2022-11-25 15:45:22 +0700

    [Models][DB] PieceOfFurnitureHistoryEntry

---
+++ app/Models/PieceOfFurnitureHistoryEntry.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\PieceOfFurnitureHistoryEntry
+ *
+ * @property int $piece_of_furniture_id
+ * @property \Illuminate\Support\Carbon $date
+ * @property int|null $apartment_id
+ * @property int|null $room_id
+ * @method static \Illuminate\Database\Eloquent\Builder|PieceOfFurnitureHistoryEntry newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PieceOfFurnitureHistoryEntry newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PieceOfFurnitureHistoryEntry query()
+ * @method static \Illuminate\Database\Eloquent\Builder|PieceOfFurnitureHistoryEntry whereApartmentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PieceOfFurnitureHistoryEntry whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PieceOfFurnitureHistoryEntry wherePieceOfFurnitureId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PieceOfFurnitureHistoryEntry whereRoomId($value)
+ * @mixin \Eloquent
+ */
+class PieceOfFurnitureHistoryEntry extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $dates = [
+        'date'
+    ];
+}
---
+++ database/migrations/2022_11_25_082615_create_piece_of_furniture_history_entries_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('piece_of_furniture_history_entries', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('piece_of_furniture_id');
+            $table->timestamp('date');
+            $table->unsignedBigInteger('apartment_id')->nullable();
+            $table->unsignedBigInteger('room_id')->nullable();
+
+            $table->foreign('piece_of_furniture_id')
+                ->references('id')
+                ->on('piece_of_furniture')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->foreign('apartment_id')
+                ->references('id')
+                ->on('apartments')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->foreign('room_id')
+                ->references('id')
+                ->on('rooms')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('piece_of_furniture_history_entries');
+    }
+};
