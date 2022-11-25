ad1768107b382dd8a444c396cac1024b2d53f2ae
Vladislav Kolegov <v.kolegov@outlook.com>
2022-11-25 14:04:23 +0700

    [Helpers] API/AbstractFieldFilter

---
+++ app/Helpers/API/AbstractFieldFilter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Helpers\API;
+
+abstract class AbstractFieldFilter implements FieldFilter
+{
+    protected string $field;
+    protected string $fieldType;
+
+    protected const FIELD_TYPES = ['int', 'string', 'date'];
+
+    /**
+     * @param string $field database column name
+     * @param string $fieldType possible values: 'int', 'string', 'date'
+     */
+    public function __construct(string $field, string $fieldType)
+    {
+        $this->field = $field;
+
+        if (!in_array($fieldType, static::FIELD_TYPES)) {
+            throw new \InvalidArgumentException("invalid \$fieldType: $fieldType");
+        }
+        $this->fieldType = $fieldType;
+    }
+}
