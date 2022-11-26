df1e33bb2ba3a23b7d51495a108b3a7c8655ae6b
Vladislav Kolegov <v.kolegov@outlook.com>
2022-11-26 22:38:26 +0700

    [Vue] RoomFurniture: filtering by date

--- resources/js/RoomFurniture.vue
+++ resources/js/RoomFurniture.vue
@@ -1,6 +1,8 @@
 <script setup>
 import API from "./services/API";
 import {ref, watch} from "vue";
+import DatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
 
 const props = defineProps({
     roomId: [Number, String],
@@ -13,23 +15,53 @@ const furnitureAPI = new API("/api/v1/pieces-of-furniture");
 const furniture = ref([]);
 
 async function fetchFurniture() {
-    const response = await furnitureAPI.index({
-        page: 1,
-        per_page: 50,
-        apartment_id: props.apartmentId ? [props.apartmentId] : null,
-        room_id: props.roomId ? [props.roomId] : null,
-    });
+    const response = await furnitureAPI.index(filterParams.value);
     furniture.value = response.entities;
 }
-fetchFurniture();
 
 watch(() => props.apartmentId, fetchFurniture);
 watch(() => props.roomId, fetchFurniture);
 
+// filtering
+
+const filterParams = ref({
+    page: 1,
+    per_page: 50,
+    apartment_id: props.apartmentId ? [props.apartmentId] : null,
+    room_id: props.roomId ? [props.roomId] : null,
+    date: (new Date()).toISOString(),
+});
+
+watch(filterParams, function() {
+   fetchFurniture();
+}, {deep: true});
+
+// filtering end
+
+// on create
+fetchFurniture(filterParams.value);
 </script>
 
 <template>
 <h1>Квартира {{ apartmentId }} | Комната {{ roomId }}</h1>
+<date-picker
+    v-model="filterParams.date"
+    format="dd/MM/yyyy HH:mm"
+    :clearable="false"
+
+    :enable-time-picker="true"
+    minutes-increment="5"
+
+
+    :month-change-on-scroll="false"
+    :auto-apply="true"
+    locale="ru-RU"
+
+    input-class-name="form-control"
+    utc="preserve"
+
+    v-bind="$attrs"
+/>
 <table class="table table-striped">
     <thead>
     <tr>
